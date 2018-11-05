43a875db6720f201b68facf53c28e65f3b2ee69f
Auwal MS <auwalforall@gmail.com>
2018-11-05 22:20:02 +0100

    Feat: added and updated the speakers page with a few speakers details.

--- src/components/speakers.vue
+++ src/components/speakers.vue
@@ -11,7 +11,7 @@
                 <div class="container has-text-left">
                     <img style="width: 200px;" id="team-img" src="../assets/logos/logo-white.svg" alt="">
                     <p class="title">
-                        Meet the Team
+                        Meet Our Speakers
                     </p>
                 </div>
             </div>
@@ -25,20 +25,7 @@
             <div class="hero-body">
                 <div class="container">
                     <div style="font-size: 14px" class=" has-text-justified">
-                        <p> Google is known all around the world. Everyone is 'googling',
-                            checking on 'maps' and communicating in 'gmail'. For simple
-                            users, they are services that just works, but not for us.
-                            Developers see much more: APIs, scalability issues, complex technology stacks.
-                            And that is what GDG is about.
-                        </p>
-                        <br>
-                        <p>
-                            <a href="">GDG North CentralDevFest</a> - is open and volunteer geek community who create exciting
-                            projects and share experience about Google technologies with a passion.
-                        </p>
-                        <br>
-                        <p>
-                            Our goal is to organize space to connect the best industry experts with our community audience to boost development of IT in Nigeria.
+                        <p> Hear from the Googlers, Partners, and Guest Speakers who are building the future with Cloud, Web, Android and Machine Learning. Anticipate amazing sessions from our speakers! Check back to see as we add more partners.
                         </p>
 
                     </div>
@@ -52,59 +39,42 @@
                         <br>
                         <div class="member-card-image">
                         <figure class="image is-128x128">
+                                <img class="is-rounded" src="../assets/people/fola.jpeg">
+                            </figure>
+                        </div>
+                        <h1 style="font-size: 24px">Fola Olatunji-David</h1>
+                        <p>Google, Nigeria</p>
+                        <p>
+                            Fola leads the Subsaharan Africa Startup Launchpad team for at Google where he helps startups succeed.
+                        </p>
+                        <p>
+                        <a class="is-info" style="padding:5px" data-social-network="Twitter"
+                        target="_blank" href="https://twitter.com/folasanwo">
+                            <b-icon
+                                icon="twitter"
+                                size="is-small">
+                            </b-icon>
+                        </a>
+                        </p>
+                        <br>
+                    </div>
+                </div>
+                <div class="column ">
+                    <div class="member-card">
+                        <br>
+                        <div class="member-card-image">
+                        <figure class="image is-128x128">
                                 <img class="is-rounded" src="../assets/people/bulama.jpg">
                             </figure>
                         </div>
                         <h1 style="font-size: 24px">Bulama Yusuf</h1>
-                        <img class="gdg-logo" src="../assets/logos/gdg-logo.png" alt="">
-                        <p>Abuja, Nigeria</p>
+                        <p>IntApps, Nigeria</p>
                         <p>
-                            <a  style="color: #4d4d4d; padding:5px" target="_blank" href="https://github.com/bulsyusuf">
-                                <b-icon
-                                    pack="fab"
-                                    icon="github"
-                                    size="is-small">
-                                </b-icon>
-                            </a>
-                            <a class="is-info" style="padding:5px" data-social-network="Twitter"
-                            target="_blank" href="https://twitter.com/bulsyusuf">
-                                <b-icon
-                                    icon="twitter"
-                                    size="is-small">
-                                </b-icon>
-                            </a>
+                            Bulama is the Founder/CEO of Intellectual Apps, a DevShop centered on building mobile and enterprise solutions
                         </p>
-                        <br>
-                    </div>
-                </div>
-                <div class="column ">
-                    <div class="member-card">
-                        <br>
-                        <div class="member-card-image">
-                        <figure class="image is-128x128">
-                                <img class="is-rounded" src="../assets/people/auwal.jpg">
-                            </figure>
-                        </div>
-                        <h1 style="font-size: 24px">Auwal MS</h1>
-                        <img class="gdg-logo" src="../assets/logos/gdg-logo.png" alt="">
-                        <p>Lafia-Abuja, Nigeria</p>
                         <p>
-                            <a  style="color: #4d4d4d; padding:5px" target="_blank" href="https://github.com/auwalms">
-                                <b-icon
-                                    pack="fab"
-                                    icon="github"
-                                    size="is-small">
-                                </b-icon>
-                            </a>
-                        <a  style="color: #3B5998; padding:5px" data-social-network="Facebook"
-                        target="_blank" href="https://web.facebook.com/auwalms">
-                            <b-icon
-                                icon="facebook"
-                                size="is-small">
-                            </b-icon>
-                        </a>
                         <a class="is-info" style="padding:5px" data-social-network="Twitter"
-                        target="_blank" href="https://twitter.com/_auwalms">
+                        target="_blank" href="https://twitter.com/bulsyusuf">
                             <b-icon
                                 icon="twitter"
                                 size="is-small">
@@ -119,66 +89,17 @@
                         <br>
                         <div class="member-card-image">
                         <figure class="image is-128x128">
-                                <img class="is-rounded" src="../assets/people/umar.png">
+                                <img class="is-rounded" src="../assets/logos/gdg-logo.png">
                             </figure>
                         </div>
-                        <h1 style="font-size: 24px">Umar Saidu Auna</h1>
-                        <img class="gdg-logo" src="../assets/logos/gdg-logo.png" alt="">
-                        <p>Minna, Nigeria</p>
+                        <h1 style="font-size: 24px">Robert John</h1>
+                        <p>EnterFive, Nigeria</p>
                         <p>
-                            <a  style="color: #4d4d4d; padding:5px" target="_blank" href="https://github.com/umarauna">
-                                <b-icon
-                                    pack="fab"
-                                    icon="github"
-                                    size="is-small">
-                                </b-icon>
-                            </a>
-                        <a  style="color: #3B5998; padding:5px" data-social-network="Facebook"
-                        target="_blank" href="https://web.facebook.com/umarauna">
-                            <b-icon
-                                icon="facebook"
-                                size="is-small">
-                            </b-icon>
-                        </a>
-                        <a class="is-info" style="padding:5px" data-social-network="Twitter"
-                        target="_blank" href="https://twitter.com/umarauna">
-                            <b-icon
-                                icon="twitter"
-                                size="is-small">
-                            </b-icon>
-                        </a>
+                            Robert is a Google Developer Expert for Machine Learning, He serves as the CTO of CoLab_KD and EnterFive.
                         </p>
-                        <br>
-                    </div>
-                </div>
-                <div class="column ">
-                    <div class="member-card">
-                        <br>
-                        <div class="member-card-image">
-                        <figure class="image is-128x128">
-                                <img class="is-rounded" src="../assets/people/usman.jpeg">
-                            </figure>
-                        </div>
-                        <h1 style="font-size: 24px">Usman Idris</h1>
-                        <img class="gdg-logo" src="../assets/logos/gdg-logo.png" alt="">
-                        <p>Keffi, Nigeria</p>
                         <p>
-                            <a  style="color: #4d4d4d; padding:5px" target="_blank" href="https://github.com/usmanu">
-                                <b-icon
-                                    pack="fab"
-                                    icon="github"
-                                    size="is-small">
-                                </b-icon>
-                            </a>
-                        <a  style="color: #3B5998; padding:5px" data-social-network="Facebook"
-                        target="_blank" href="https://web.facebook.com/usmanu">
-                            <b-icon
-                                icon="facebook"
-                                size="is-small">
-                            </b-icon>
-                        </a>
                         <a class="is-info" style="padding:5px" data-social-network="Twitter"
-                        target="_blank" href="https://twitter.com/usmanu">
+                        target="_blank" href="https://twitter.com/bulsyusuf">
                             <b-icon
                                 icon="twitter"
                                 size="is-small">
