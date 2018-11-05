fa7ee2945924db2fe927a8aa3d3f430411479dc0
Auwal MS <auwalforall@gmail.com>
2018-11-05 07:35:20 +0100

    fix: updated the map to the new location

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="map" style="margin-top: 100px;">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.294761576666!2d7.494947614293468!3d9.03685349351543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b8756a909c3%3A0x3eda62fe55f6387b!2sAyalla+Hotels!5e0!3m2!1sen!2sng!4v1539777840614" 
-        width="100%" height="450" 
-        frameborder="0" 
+        <iframe
+        src="https://maps.google.com/maps?q=IBI%20Training%20Center&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        width="100%" height="450"
+        frameborder="0"
         style="border:0" allowfullscreen>
         </iframe>
     </section>
+    <!-- https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.294761576666!2d7.494947614293468!3d9.03685349351543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b8756a909c3%3A0x3eda62fe55f6387b!2sAyalla+Hotels!5e0!3m2!1sen!2sng!4v1539777840614 -->
 </template>
